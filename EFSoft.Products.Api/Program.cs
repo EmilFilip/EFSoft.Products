2130000a44d2af71b45fbb669fa7bd9eb8f078a4
Emil Filip <emilfilip@hotmail.com>
2023-10-24 20:04:40 +0100

    Added health check

--- EFSoft.Products.Api/Program.cs
+++ EFSoft.Products.Api/Program.cs
@@ -1,23 +1,24 @@
 var builder = WebApplication.CreateBuilder(args);
 
-if (!builder.Environment.IsDevelopment())
-{
-    var appConfigurationConnectionString = builder.Configuration.GetValue<string>("AppConfigurationConnectionString");
+//if (!builder.Environment.IsDevelopment())
+//{
+//    var appConfigurationConnectionString = builder.Configuration.GetValue<string>("AppConfigurationConnectionString");
 
-    builder.Configuration.AddAzureAppConfiguration(options =>
-    {
-        options.Connect(appConfigurationConnectionString)
-                .ConfigureRefresh(refresh =>
-                {
-                    refresh.Register("Settings:Sentinel", refreshAll: true).SetCacheExpiration(new TimeSpan(0, 1, 0));
-                });
-    });
-}
+//    builder.Configuration.AddAzureAppConfiguration(options =>
+//    {
+//        options.Connect(appConfigurationConnectionString)
+//                .ConfigureRefresh(refresh =>
+//                {
+//                    refresh.Register("Settings:Sentinel", refreshAll: true).SetCacheExpiration(new TimeSpan(0, 1, 0));
+//                });
+//    });
+//}
 
 // Add services to the container.
 builder.Services.AddAuthorization();
 builder.Services.AddEndpointsApiExplorer();
 builder.Configuration.AddEnvironmentVariables();
+builder.Services.AddHealthChecks();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddSwaggerGen(c =>
@@ -25,11 +26,12 @@ builder.Services.AddSwaggerGen(c =>
     c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo { Title = "Products Microservice", Version = "v1" });
 });
 
-builder.Services.RegisterLocalServices(builder.Configuration);
+//builder.Services.RegisterLocalServices(builder.Configuration);
 
 var app = builder.Build();
 
-app.MapProductEndpoints();
+//app.MapProductEndpoints();
+app.MapHealthChecks("/health");
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
